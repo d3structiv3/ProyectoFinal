98dfcceacd46aed48ac0e1edd3d6da1473786bba
Mario Alberto <marioalberto220298@hotmail.com>
2020-12-03 20:32:13 -0600

    Diseño de Login

--- View/Login/login.php
+++ View/Login/login.php
@@ -1,1 +1,48 @@
-<h1>Aqui va el login</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
+
+</head>
+
+<body>
+    <main>
+        <div class="container ">
+            <div class="row">
+                <div class="col-sm-6 login-section-wrapper">
+
+                    <div class="login-wrapper my-auto ">
+                        <h1 class="display-3" style="text-align:center">Entrar</h1>
+                        <form action="#!">
+                            <div class="form-group">
+                                <label for="user">Usuario</label>
+                                <input name="user" id="user" class="form-control" placeholder="Ingresa tu usuario">
+                            </div>
+                            <div class="form-group mb-4">
+                                <label for="password">Contraseña</label>
+                                <input type="password" name="password" id="password" class="form-control" placeholder="Ingresa tu contraseña">
+                            </div>
+                            <input name="login" id="login" class="btn btn-block login-btn btn-primary" type="button" value="Entrar">
+                        </form>
+                        <br />
+                        <p class="login-wrapper-footer-text">No tienes una cuenta? <a href="../../view/login/registro.php" class="btn btn-outline-info">Registrate aquí</a> <a href="../../../ProyectoFinal/index.php" class="btn btn-outline-danger">Regresar</a></p>
+                    </div>
+                </div>
+                <div class="col-sm-6 px-0 d-none d-sm-block px-5">
+                    <img src="https://www.flaticon.es/svg/static/icons/svg/2643/2643368.svg">
+                </div>
+            </div>
+        </div>
+    </main>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,88 +1,88 @@
 <?php
-    include './src/Layout/menu.php' ;
+include './src/Layout/menu.php';
 ?>
 
 <div class="mt-3 slide">
-        <div class="container">
-            
-                <div align="center">
-                    <img src="https://www.flaticon.es/svg/static/icons/svg/3606/3606117.svg" width="300px" height="300px" />
-                </div>
-                <div align="center">
-                    <h2>¿Qué es EduConnect?</h2>
-                    <p style="text-align:justify">
-                        EduConnect es la plataforma de ayuda para la comunicación entre los docentes
-                        y los padres de familia en lo relacionado a la vida académica de los alumnos a nivel primaria, y de esta manera adaptarse
-                        a la nueva normalidad pero no afectando la comunicación entre maestros y alumnos
+    <div class="container">
 
-                    </p>
-                </div>
+        <div align="center">
+            <img src="https://www.flaticon.es/svg/static/icons/svg/3606/3606117.svg" width="300px" height="300px" />
+        </div>
+        <div align="center">
+            <h2>¿Qué es EduConnect?</h2>
+            <p style="text-align:justify">
+                EduConnect es la plataforma de ayuda para la comunicación entre los docentes
+                y los padres de familia en lo relacionado a la vida académica de los alumnos a nivel primaria.
+
+
+            </p>
         </div>
     </div>
-    <div class="container-md">
-        <div class="row">
+</div>
+<div class="container-md">
+    <div class="row">
+        <div class="col">
+            <div align="center">
+                <img src="https://www.flaticon.es/svg/static/icons/svg/906/906175.svg" width="100px" height="100px" />
+            </div>
+            <h2>Importancia de EduConnect</h2>
+            <p style="text-align:justify">
+                Si estás pensando en la administración académica virtual pero que
+                por cuestiones de tiempo o distancia no puedes juntar a todo el mundo en un mismo espacio; todos
+                pueden aprovechar la enseñanza online desde la innovación.
+
+            </p>
+        </div>
+        <div class="col">
+            <div align="center">
+                <img src="https://www.flaticon.es/svg/static/icons/svg/3402/3402135.svg" width="100px" height="100px" />
+            </div>
+            <h2>¿Cómo funciona?</h2>
+            <p style="text-align:justify">
+                Padres de familia: mediante esta plataforma podrán recibir las calificaciones de sus hijos y de la misma manera mejorar
+                la interacción con los Profesores de los estudiantes
+                <br />
+                Profesores: Podrán subir las boletas de calificaciones de sus alumnos para que de esta manera los padres
+                puedan tener mejor comunicación con ustedes sobre la vida académica de sus hijos.
+
+            </p>
+        </div>
+        <div class="w-100"></div>
+        <div class="col">
             <div class="col">
                 <div align="center">
-                    <img src="https://www.flaticon.es/svg/static/icons/svg/906/906175.svg" width="100px" height="100px" />
+                    <img src="https://www.flaticon.es/svg/static/icons/svg/3626/3626838.svg" width="100px" height="100px" />
                 </div>
-                <h2>¿Qué es EduConnect?</h2>
+                <h2>Conexión con EduConnect</h2>
                 <p style="text-align:justify">
-                    EduConnect es la plataforma de ayuda para la comunicación entre los docentes
-                    y los padres de familia en lo relacionado a la vida académica de los alumnos a nivel primaria, y de esta manera adaptarse
-                    a la nueva normalidad pero que no cambie la conexión entre estos
+                    En el caso de la educación básica lo virtual entró como un eje de la enseñanza y dependiendo del acceso a
+                    la tecnología e Internet, algunas escuelas proponen muchas de sus actividades y evaluaciones de manera
+                    virtual. Incluso a través de sistemas que permiten el seguimiento del aprendizaje por parte de los padres.
 
+                    De cualquier forma, es ya bien sabido que las generaciones más jóvenes están tan familiarizadas con
+                    la tecnología que ya es parte de la vida cotidiana y no tiene sentido dejar de aprovecharla en la educación.
                 </p>
             </div>
+        </div>
+        <div class="col">
             <div class="col">
                 <div align="center">
-                    <img src="https://www.flaticon.es/svg/static/icons/svg/3402/3402135.svg" width="100px" height="100px" />
+                    <img src="https://www.flaticon.es/svg/static/icons/svg/3612/3612559.svg" width="100px" height="100px" />
                 </div>
-                <h2>¿Cómo funciona?</h2>
+                <h2>Interacción</h2>
                 <p style="text-align:justify">
-                    Padres de familia: mediante esta plataforma podrán recibir las calificaciones de sus hijos
-                    Profesores: Podrán subir las boletas de calificaciones de sus alumnos
-
+                    Por el hecho de ser virtual, EduConnect cuenta con diferentes características, semejanzas y diferencias que 
+                    las hacen ser geniales para contenido online o virtual, de esta manera se puede decir que la novedad 
+                    vino por el lado de la tecnología.
                 </p>
-            </div>
-            <div class="w-100"></div>
-            <div class="col">
-                <div class="col">
-                    <div align="center">
-                        <img src="https://www.flaticon.es/svg/static/icons/svg/3626/3626838.svg" width="100px" height="100px" />
-                    </div>
-                    <h2>Lorem</h2>
-                    <p style="text-align:justify">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Mauris in aliquam sem fringilla ut morbi tincidunt augue. Aenean euismod elementum nisi quis eleifend. Fusce ut placerat orci 
-                        nulla pellentesque dignissim. Ipsum consequat nisl vel pretium lectus quam id. Integer malesuada nunc vel risus commodo viverra 
-                        maecenas. Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Vulputate mi sit amet mauris commodo. Faucibus
-                        in ornare quam viverra orci sagittis. Aliquam purus sit amet luctus venenatis. Est sit amet facilisis magna.
-
-                    </p>
-                </div>
-            </div>
-            <div class="col">
-                <div class="col">
-                    <div align="center">
-                        <img src="https://www.flaticon.es/svg/static/icons/svg/3612/3612559.svg" width="100px" height="100px" />
-                    </div>
-                    <h2>Lorem</h2>
-                    <p style="text-align:justify">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Mauris in aliquam sem fringilla ut morbi tincidunt augue. Aenean euismod elementum nisi quis eleifend. Fusce ut placerat orci
-                        nulla pellentesque dignissim. Ipsum consequat nisl vel pretium lectus quam id. Integer malesuada nunc vel risus commodo viverra
-                        maecenas. Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Vulputate mi sit amet mauris commodo. Faucibus
-                        in ornare quam viverra orci sagittis. Aliquam purus sit amet luctus venenatis. Est sit amet facilisis magna.
-
-                    </p>
-                </div>
             </div>
         </div>
     </div>
+</div>
 
 
 
 
 <?php
-    include './src/Layout/footer.php' ;
+include './src/Layout/footer.php';
 ?>
